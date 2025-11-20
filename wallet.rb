27f180491b83945ba26b2e5dacd0de98f75e78e8
RaJharit77 <hei.rajo.2@gmail.com>
2025-11-20 14:35:54 +0300

    feat: add Gemfile, Bundler setup, RuboCop config, and Ruby wallet script

---
+++ wallet.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Wallet
+  attr_accessor :color, :size
+
+  def initialize(color, size)
+    @color = color
+    @size = size
+    @balance = 0
+    @isOpen = false
+    @isLost = false
+  end
+
+  def addVola(amount)
+    @balance += amount
+  end
+
+  def getVola
+    @balance
+  end
+
+  def open
+    @isOpen = true
+  end
+
+  def close
+    @isOpen = false
+  end
+
+  def isLost
+    @isLost
+  end
+
+  def checkVola
+    @balance
+  end
+end
